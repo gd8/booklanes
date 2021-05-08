9a9f6cb42f6550bfe645a20f80802c005dc36478
gd8 <gdanielsson@me.com>
2021-05-08 08:45:04 -0400

    add NotFound component

---
+++ src/shared/not-found.jsx
@@ -0,0 +1,10 @@
+export const NotFound = () => {
+  return (
+    <article className='message half is-danger'>
+      <div className='message-header'>
+        <p>Error</p>
+      </div>
+      <div className='message-body'>Book not found.</div>
+    </article>
+  );
+};
