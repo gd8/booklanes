7f8d66466a85fc4e8cb02f2779fdeb50ef2034ca
gd8 <gdanielsson@me.com>
2021-05-08 08:47:20 -0400

    complete MVP for books CRUD

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { NewBook } from './new-book/new-book';
+import { Books } from './books/books';
+import { BookDetail } from './book-detail/book-detail';
+import { NotFound } from './shared/not-found';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    const storedBooks = localStorage.getItem('books');
+    this.state = {
+      books: storedBooks ? JSON.parse(storedBooks) : [],
+    };
+  }
+
+  saveBooks(books) {
+    this.setState({
+      books,
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  onNewBookSubmit(form) {
+    const newBooks = [
+      ...this.state.books,
+      { ...form, status: 0, id: this.state.books.length },
+    ];
+    this.saveBooks(newBooks);
+  }
+
+  updateBook(updatedBook) {
+    const updatedBooks = this.state.books.map((book) => {
+      if (updatedBook.id === book.id) {
+        return {
+          ...book,
+          ...updatedBook,
+        };
+      }
+      return book;
+    });
+    this.saveBooks(updatedBooks);
+  }
+
+  deleteBook(bookToDelete) {
+    const updatedBooks = this.state.books.filter((book) => {
+      return book.id !== bookToDelete.id;
+    });
+    this.saveBooks(updatedBooks);
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <nav className='navbar'>
+            <ul className='navbar-start'>
+              <li className='navbar-item'>
+                <Link to='/'>Home</Link>
+              </li>
+              <li className='navbar-item'>
+                <Link to='/new'>New Book</Link>
+              </li>
+              <li className='navbar-item'>
+                <Link to='/books'>Books</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path='/new'>
+              <NewBook onNewBookSubmit={this.onNewBookSubmit.bind(this)} />
+            </Route>
+            <Route exact path='/books'>
+              <Books
+                books={this.state.books}
+                updateBook={this.updateBook.bind(this)}
+              />
+            </Route>
+            <Route
+              path='/books/:id'
+              render={(props) => {
+                const bookId = props.match.params.id;
+                const book = this.state.books.find(
+                  (book) => String(book.id) === String(bookId)
+                );
+                return book ? (
+                  <BookDetail
+                    book={book}
+                    updateBook={this.updateBook.bind(this)}
+                    deleteBook={this.deleteBook.bind(this)}
+                    history={props.history}
+                  />
+                ) : (
+                  <NotFound />
+                );
+              }}
+            ></Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
