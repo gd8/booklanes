5bd565c19430cff50e62c49860908ed7d0523b5f
gd8 <gdanielsson@me.com>
2021-06-08 19:34:32 -0400

    add consts file for lanes definition

---
+++ src/shared/lanes.ts
@@ -0,0 +1,18 @@
+export const Lanes = [
+  {
+    id: '0',
+    description: 'To Read'
+  },
+  {
+    id: '1',
+    description: 'Reading'
+  },
+  {
+    id: '2',
+    description: 'Read'
+  },
+  {
+    id: '3',
+    description: 'Noped'
+  },
+]
