65cd135104ee24dab020715876919b446d979198
gd8 <gdanielsson@me.com>
2021-06-08 19:49:12 -0400

    move book detail under pages

--- src/pages/book-detail/book-detail.jsx
+++ src/pages/book-detail/book-detail.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
-import { BookForm } from '../shared/book-form';
-import { ConfirmModal } from '../shared/confirm-modal';
+import { BookForm } from '../../shared/components/book-form';
+import { ConfirmModal } from '../../shared/confirm-modal';
 
 export class BookDetail extends Component {
   constructor(props) {
