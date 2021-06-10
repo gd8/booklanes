1f3c75e124aa27f35519c0f73af70839b7cfc1d2
gd8 <gdanielsson@me.com>
2021-06-10 08:59:26 -0400

    move navbar component and remove home

--- src/shared/components/navbar.jsx
+++ src/shared/components/navbar.jsx
@@ -7,11 +7,6 @@ export const Navbar = () => {
         <div className='navbar-brand'>
           <ul className='navbar-start'>
             <li className='navbar-item'>
-              <Link to='/' className='is-capitalized has-text-weight-bold'>
-                Home
-              </Link>
-            </li>
-            <li className='navbar-item'>
               <Link to='/books' className='is-capitalized has-text-weight-bold'>
                 Books
               </Link>
